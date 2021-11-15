9e5655d7418e226cd4cadcf990a23355105e1290
Abbosjon Keldiyarov <abbosjon99@gmail.com>
2021-11-15 22:01:06 +0500

    Create Task3.py

---
+++ Lesson 17/Task3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 21:55:48 2021
+
+@author: abbos
+"""
+
+savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+savol += "Musbat son kiriting "
+savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+while True:
+    qiymat = input(savol)
+    if qiymat.lower()=='exit':
+        break
+    elif int(qiymat)<0:
+        continue
+    else:
+        ildiz = float(qiymat)**(0.5)
+        print(f"{qiymat} ning ildizi {ildiz} ga teng")
